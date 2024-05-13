ab286cbe9af3cf69bbf7d14e7842e29ac8ce95a7
zhangwencheng <zhangwencheng@shantaijk.cn>
2024-05-13 17:35:29 +0800

    udpate

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AppRouter from './router/router';
 import './asset/styles/iconfont.css'
 import './asset/styles/public.scss'
 
+function App() {
+  return (
+      <div className="App">
+          <AppRouter />
+      </div>
+  );
+}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
