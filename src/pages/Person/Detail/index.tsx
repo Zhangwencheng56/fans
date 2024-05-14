9a656af75df7b38cc6db2d02517ecfaf0844aa20
zhangwencheng <zhangwencheng@shantaijk.cn>
2024-05-14 10:59:42 +0800

    update

--- src/pages/Person/Detail/index.tsx
+++ src/pages/Person/Detail/index.tsx
@@ -1,7 +1,15 @@
+import { Avatar } from "antd-mobile";
 import React from "react";
 
 const PersonDetail = () => {
-  return <div className="container_person_detail"></div>;
+  return (
+    <div className="container_person_detail">
+      <Avatar src="https://images.unsplash.com/photo-1567945716310-4745a6b7844b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=60"></Avatar>
+      <div className="that_detail">
+        
+      </div>
+    </div>
+  );
 };
 
 export default PersonDetail;
